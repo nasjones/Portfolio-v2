9d41ba2d56230291b7390ba57ddcea8348830125
nasjones <nasjonez1@gmail.com>
2019-11-07 07:38:28 -0500

    transition updated to swipe

---
+++ Store.js
@@ -0,0 +1,34 @@
+let bioHtml = `<section id="display1" class="displayed">
+            <h2>About Me</h2>
+            <p id="bio" class="body-text">I’m a web developer/foodie who’s passion for development came from my love of
+                critical thinking
+                and
+                problem-solving. This is the perfect field to indulge my self in something I love to do. When I'm
+                not playing with some algorithm on my computer I'm playing video games, eating with friends, or reading.
+                My
+                favorite genre is Science-fiction I hope to be apart of the development that brings forth the future I
+                love
+                reading about.
+            </p>
+        </section>`;
+
+let workHtml = `<section id="display2" class="displayed">
+        <h2>My Work</h2>
+        <article class="work" id="item-1">
+            <h3>My personal FullMetal quiz</h3>
+            <a href="https://nasjones.github.io/quiz-app/" target="_blank" class="nonClick">
+                 <img class="work-photo" src="quiz-app-cap.png" alt="quiz-app photo">
+            </a>
+            <p class="body-text">This is a quiz that I designed based on the tv-show FullMetal Alchemist. 
+            I designed the quiz so that the questions and choices for the questions appear in a random order so that the quiz isn't the same every time.
+            This project showcases my skills in Javascript/Jquery/CSS/HTML.</p>
+            <ul>
+                <li>live- <a href="https://nasjones.github.io/quiz-app/" target="_blank" class="nonClick">Take the quiz!</a>
+                </li>
+                <li>repo- <a href="https://github.com/nasjones/quiz-app" target="_blank" class="nonClick">Checkout the files.</a>
+                </li>
+             </ul>
+        </article>
+    </section>`;
+
+let moreHtml = ``
--- index-java.js
+++ index-java.js
@@ -32,40 +32,71 @@ function hamMenu() {
 
 // $(begin)
 
-function leftOut() {
+function textOut() {
+    $('#stage').on('swipeleft', e => {
+        $(e.currentTarget).children().animate({
+            left: '-100%'
+        }, 500);
+        setTimeout(stageUpdate, 800);
+
+    });
+}
+
+function stageUpdate() {
+    let staged = $('#stage').find('.displayed').attr('id');
+    console.log(staged);
+    if (staged == 'display1') {
+        $('#stage').html(workHtml);
+        $('#stage').find('.displayed').css('left', '150%');
+        $('.displayed').animate({
+            left: '0%'
+        }, 500);
+    }
+    else if (staged == 'display2') {
+
+    }
+    else {
+        $('#stage').html(bioHtml);
+        $('#stage').find('.displayed').css('left', '150%'), 500;
+        $('.displayed').animate({
+            left: '0%'
+        }, 500);
+    }
 
 }
 
-$('.body-text').click(function () {
+// $('.body-text').click(function () {
 
-    $('.displayed').animate({
-        left: '-100%'
-    }, 500, function () {
-        $('#stage').html(`<section id="display2" class="displayed">
-            <article class="work" id="item-1">
-                <h3>My personal FullMetal quiz</h3>
-                <a href="https://nasjones.github.io/quiz-app/" target="_blank">
-                    <img class="work-photo" src="quiz-app-cap.png" alt="quiz-app photo">
-                </a>
-                <p class="body-text">This is a quiz that I designed based on the tv-show FullMetal Alchemist. I designed
-                    the quiz so that
-                    the questions and choices for the questions appear in a random order so that the quiz isn't the same
-                    every
-                    time.
-                    This project showcases my skills in Javascript/Jquery/CSS/HTML.</p>
-                <ul>
-                    <li>live- <a href="https://nasjones.github.io/quiz-app/" target="_blank">Take the quiz!</a>
-                    </li>
-                    <li>repo- <a href="https://github.com/nasjones/quiz-app" target="_blank">Checkout the files.</a>
-                    </li>
-                </ul>
-            </article>
-        </section>`);
-        $('.displayed').css('left', '150%');
-        // $('.displayed').appendTo('#stage');
-    });
+//     $('.displayed').animate({
+//         left: '-100%'
+//     }, 500, function () {
+//         $('#stage').html(`<section id="display2" class="displayed">
+//             <article class="work" id="item-1">
+//                 <h3>My personal FullMetal quiz</h3>
+//                 <a href="https://nasjones.github.io/quiz-app/" target="_blank">
+//                     <img class="work-photo" src="quiz-app-cap.png" alt="quiz-app photo">
+//                 </a>
+//                 <p class="body-text">This is a quiz that I designed based on the tv-show FullMetal Alchemist. I designed
+//                     the quiz so that
+//                     the questions and choices for the questions appear in a random order so that the quiz isn't the same
+//                     every
+//                     time.
+//                     This project showcases my skills in Javascript/Jquery/CSS/HTML.</p>
+//                 <ul>
+//                     <li>live- <a href="https://nasjones.github.io/quiz-app/" target="_blank">Take the quiz!</a>
+//                     </li>
+//                     <li>repo- <a href="https://github.com/nasjones/quiz-app" target="_blank">Checkout the files.</a>
+//                     </li>
+//                 </ul>
+//             </article>
+//         </section>`);
+//         $('.displayed').css('left', '150%');
+//         // $('.displayed').appendTo('#stage');
+//     });
 
-    $('.displayed').animate({
-        left: '0%'
-    }, 500);
-});
+//     $('.displayed').animate({
+//         left: '0%'
+//     }, 500);
+// });
+
+$(textOut);
