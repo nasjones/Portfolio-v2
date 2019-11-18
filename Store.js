89952a0e7dbea17fa0204570256d4b371c51d9e0
nasjones <nasjonez1@gmail.com>
2019-11-18 09:02:55 -0500

    alt fix

--- Store.js
+++ Store.js
@@ -1,24 +1,19 @@
 let bioHtml = `<article id="display2" class="displayed">
             <h2>About Me</h2>
-            <p id="bio" class="body-text"><span>I was born and raised in Long Island New York. I’m a web developer/foodie who’s passion for development came from my love of
-                critical thinking
-                and
-                problem-solving. This is the perfect field to indulge my self in something I love to do. When I'm
+            <p id="bio" class="body-text">I was born and raised in Long Island New York. I’m a web developer/foodie who’s passion for development came from my love of
+                critical thinking and problem-solving. This is the perfect field to indulge my self in something I love to do. When I'm
                 not playing with some algorithm on my computer I'm playing video games, eating with friends, or reading.
-                My
-                favorite genre is Science-fiction I hope to be apart of the development that brings forth the future I
-                love
-                reading about.
+                My favorite genre is Science-fiction I hope to be apart of the development that brings forth the future I love reading about.
             </p>
         </article>`;
 
 let workHtml = `<article id="display1" class="displayed">
         <h2>My Work</h2>
-        <section class="work" id="item-1" >
-            <h3>My personal FullMetal quiz</h3>
-            <a href="https://nasjones.github.io/quiz-app/" target="_blank" class="nonClick">
-            <section class="content" id="quiz" style="background-image: url('quiz-app-cap.png');">
-            </section>
+        <section class="work" id="item-1">
+            <h3>FullMetal quiz</h3>
+            <a href="https://nasjones.github.io/quiz-app/" target="_blank" class="nonClick" alt="quiz screen cap">
+            <div class="content" id="quiz" style="background-image: url('quiz-app-cap.png');">
+            </div>
             </a>
             <p class="body-text">This is a quiz that I designed based on the tv-show FullMetal Alchemist. 
             I designed the quiz so that the questions and choices for the questions appear in a random order so that the quiz isn't the same every time.
@@ -36,47 +31,47 @@ let moreHtml = `<article id="display3" class="displayed">
             <h2>A few of my favorite things:</h2>
             <ul>
                 <h3>My favorite restaurant</h3>
-                <section class="content" style="background-image: url('lukes-lobster.jpeg');">
+                <div class="content" style="background-image: url('lukes-lobster.jpeg');">
                     <div class="overlay">
                         <li>
                             <h4>Lukes-Lobster</h4>
                             <p>The best seafood rolls in New York!</p>
                         </li>
                     </div>
-                </section>
+                </div>
                 <div class="mobile-description"><p>The best seafood rolls in New York!</p></div>
                 <hr>
                 <h3>My favorite video game</h3>
-                <section class="content" style="background-image: url('KH.jpg');">
+                <div class="content" style="background-image: url('KH.jpg');">
                     <div class="overlay">
                         <li>
                             <h4>Kingdom-Hearts</h4>
                             <p>The greatest video game of all time!</p>
                         </li>
                     </div>
-                </section>
+                </div>
                 <div class="mobile-description"><p>The greatest video game of all time!</p></div>
                 <hr>
                 <h3>My favorite Book</h3>
-                <section class="content" style="background-image: url('POG.jpg');">
+                <div class="content" style="background-image: url('POG.jpg');">
                     <div class="overlay">
                         <li>
                             <h4>Player Of Games</h4>
                             <p>An incredible novel by Iain M. Banks with an intricate and beautiful story.</p>
                         </li>
                     </div>
-                </section>
+                </div>
                 <div class="mobile-description"><p>An incredible novel by Iain M. Banks with an intricate and beautiful story.</p></div>
                 <hr>
                 <h3>My favorite show</h3>
-                <section class="content" style="background-image: url('Stein.jpg');">
+                <div class="content" style="background-image: url('Stein.jpg');">
                     <div class="overlay">
                         <li>
                             <h4>Steins;Gate</h4>
                             <p>An amazing story with beautiful art, a wonderful story, and great characters.</p>
                         </li>
                     </div>
-                </section>
+                </div>
                 <div class="mobile-description"><p>An amazing story with beautiful art, a wonderful story, and great characters.</p></div>
             </ul>
 
