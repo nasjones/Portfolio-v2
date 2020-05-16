543d56a2661a40655ffb352ea7b6a88cf7a2bcd9
nasjones <nasjonez1@gmail.com>
2020-05-16 01:25:21 -0400

    scroll update

--- index-java.js
+++ index-java.js
@@ -1,83 +1,58 @@
+let closed
+var $window = $(window),
+    $stickyEl = $('#myNav'),
+    elTop = $stickyEl.offset().top;
+
 function hamMenu() {
-    var x = document.getElementById("nav-text");
-    if (x.style.display === "block") {
-        x.style.display = "none";
+    var el = document.getElementById("nav-text");
+    if (window.innerWidth >= 500) {
+        el.style.display = "grid";
     } else {
-        x.style.display = "block";
+        if (el.style.display === "block") {
+            el.style.display = "none";
+        } else {
+            el.style.display = "block";
+        }
     }
 }
 
+function myScroll(id) {
+    if (!closed)
+        closeAlert()
 
-function stageUpdate() {
-    let staged = $('#stage').find('.displayed').attr('id');
-    if (staged == 'display1') {
-        $('#stage').html(bioHtml);
-        $('#stage').find('.displayed').css('left', '150%');
-        $('.displayed').animate({
-            left: '0%'
-        }, 500);
-        $("#workRad").attr("checked", true);
-    }
-    else if (staged == 'display2') {
-        $('#stage').html(moreHtml);
-        $('#stage').find('.displayed').css('left', '150%');
-        $('.displayed').animate({
-            left: '0%'
-        }, 500);
-        $("#moreRad").attr("checked", true);
-    }
-    else {
-        $('#stage').html(workHtml);
-        $('#stage').find('.displayed').css('left', '150%');
-        $('.displayed').animate({
-            left: '0%'
-        }, 500);
-        $("#bioRad").attr("checked", true);
+    let element = document.getElementById(`display${id}`);
+    let offset = document.getElementById("myNav").offsetHeight
+    let bodyRect = document.body.getBoundingClientRect().top;
+    let elementRect = element.getBoundingClientRect().top;
+    let elementPosition = elementRect - bodyRect;
+    let offsetPosition = elementPosition - offset;
+
+    if (window.innerWidth < 500) {
+        hamMenu()
+        offsetPosition + 20
     }
 
+    window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+    });
 }
 
-
-$("input[type='radio']").click(function () {
-    if ($(window).width() < 500) {
-        hamMenu();
-    }
-    var radioId = $("input[name='display']:checked").attr('id');
-    if (radioId == 'workRad') {
-        $('#stage').children().animate({
-            left: '-150%'
-        }, 500); setTimeout(function () {
-            $('#stage').html(workHtml);
-            $('#stage').find('.displayed').css('left', '150%');
-            $('.displayed').animate({
-                left: '0%'
-            }, 500);
-        }, 800);
-    }
-    else if (radioId == 'moreRad') {
-        $('#stage').children().animate({
-            left: '-150%'
-        }, 500); setTimeout(function () {
-            $('#stage').html(moreHtml);
-            $('#stage').find('.displayed').css('left', '150%');
-            $('.displayed').animate({
-                left: '0%'
-            }, 500);
-        }, 800);
-    }
-    else {
-        $('#stage').children().animate({
-            left: '-150%'
-        }, 500); setTimeout(function () {
-            $('#stage').html(bioHtml);
-            $('#stage').find('.displayed').css('left', '150%');
-            $('.displayed').animate({
-                left: '0%'
-            }, 500);
-        }, 800);
-    }
-});
-
-$("#closeAlert").click(function () {
+function closeAlert() {
     $("#menuAlert").remove();
+    $stickyEl = $('#myNav')
+    elTop = $stickyEl.offset().top;
+    closed = true
+}
+
+$window.scroll(function () {
+
+    $stickyEl.toggleClass('sticky', $window.scrollTop() >= elTop);
+
+    if ($window.scrollTop() >= elTop) {
+        $('main').css('margin-top', $stickyEl.height());
+    }
+    else {
+        $('main').css('margin-top', 0);
+    }
 });
